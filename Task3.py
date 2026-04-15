a9383372532c5a0351bbeaac2eb87349cc22d9e3
xanderokun <xokun@calpoly.edu>
2026-04-14 19:11:24 -0700

    I did task 3

---
+++ Task3.py
@@ -0,0 +1,25 @@
+def smallest(n:float, m:float) -> float:
+    if n<m:
+        return n      # For which calls below is this statement evaluated? if n is smaller than m
+    else:
+        return m
+
+first = smallest(3,2)      # What is the value of first? 2
+second = smallest(2,2)      # What is the value of second? Is this a reasonable result? Why or why not? 2, I think this is reasonable because they are equal
+print("1st =", first, "and 2nd =", second)
+
+def function2(a:int, b:int, c:int) -> int:
+    if a > b and a > c:
+        return a - b             # In general, when will a call to this function evaluate this statement? When a > b and a > c
+    elif b > c:
+        return b + c             # In general, when will a call to this function evaluate this statement? When a < b and a < c and b > c
+    else:
+        return 2 * c             # In general, when will a call to this function evaluate this statement? When a < b and a < c and b < c
+
+
+answer1 = function2(3, 2, 1)     # What is the value of answer1? 1
+answer2 = function2(2, 3, 1)     # What is the value of answer2? 4
+answer3 = function2(2, 1, 3)     # What is the value of answer3? 6
+print("answer1 =", answer1)
+print("answer2 =", answer2)
+print("answer3 =", answer3)
